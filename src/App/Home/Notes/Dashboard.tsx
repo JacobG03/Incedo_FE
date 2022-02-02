95f2cf1defbce32968add869a3042bf2191b9351
JacobG03 <jaqobek1995@gmail.com>
2022-02-02 16:30:11 +0100

    dashboard styled

--- src/App/Home/Notes/Dashboard.tsx
+++ src/App/Home/Notes/Dashboard.tsx
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import styled, { useTheme } from "styled-components";
 import { IGetStatus, IState } from "../../../types";
+import { ReactComponent as FavoriteSVG } from '../../../assets/svg/star.svg'
+import { ReactComponent as ReverseSVG } from '../../../assets/svg/arrow-3.svg'
 
+
+const Container = styled.div`
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 0.5rem;
+  background-color: ${p => p.theme.bg};
+  border-radius: var(--border-radius);
+  color: ${p => p.theme.sub};
+  filter: var(--shadow);
+`
 
 interface Props {
   children: JSX.Element
@@ -12,6 +28,8 @@ const Dashboard = (props: Props) => {
   const [favorite, setFavorite] = useState(false)
   const [sort, setSort] = useState('timestamp')
   const [reverse, setReverse] = useState(false)
+
+  const theme = useTheme()
 
   const notes_status = useSelector<IState, IGetStatus>(state => state.notes.fetchNotes)
   const sections_status = useSelector<IState, IGetStatus>(state => state.sections.fetchSections)
@@ -27,14 +45,28 @@ const Dashboard = (props: Props) => {
   }
   return (
     <>
-      <span onClick={() => setFavorite(!favorite)}>Favorite: {`${favorite}`}</span>
-      <span>Sort method: {sort}</span>
-      <span onClick={() => setSort('timestamp')}>Timestamp</span>
-      <span onClick={() => setSort('modified')}>Modified</span>
-      <span onClick={() => setReverse(!reverse)}>Reverse: {`${reverse}`}</span>
-      {React.cloneElement(props.children, { 
+      <Container>
+        <FavoriteSVG
+          onClick={() => setFavorite(!favorite)}
+          fill={favorite ? theme.main : 'none'}
+          style={{ color: favorite ? theme.main : 'inherit' }}
+        />
+        <span 
+          onClick={() => setSort('timestamp')}
+          style={{color: sort === 'timestamp' ? theme.main: 'inherit'}}
+        >Created</span>
+        <span 
+          onClick={() => setSort('modified')}
+          style={{color: sort === 'modified' ? theme.main: 'inherit'}}
+        >Recent</span>
+        <ReverseSVG
+          onClick={() => setReverse(!reverse)}
+          style={{ color: reverse ? theme.main : 'inherit' }}
+        />
+      </Container>
+      {React.cloneElement(props.children, {
         options: options,
-        parent_id: props.parent_id ? props.parent_id: null
+        parent_id: props.parent_id ? props.parent_id : null
       })}
     </>
   )
